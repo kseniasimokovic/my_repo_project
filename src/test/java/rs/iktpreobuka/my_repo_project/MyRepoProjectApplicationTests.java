d46ba4cc0d3a364a495706c4f95a0fc67e3b5f7c
Ksenia Simokovic <ksenia.simokovic@gmail.com>
2018-12-27 20:51:36 +0100

    master

---
+++ src/test/java/rs/iktpreobuka/my_repo_project/MyRepoProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package rs.iktpreobuka.my_repo_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyRepoProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
